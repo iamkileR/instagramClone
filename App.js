f021c7bc7c866b53639f7c8d35a2a1ff92b7a0ab
iamkileR <jakubsadza@gmail.com>
2021-10-24 20:02:08 +0200

    Nowe1

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Tu kiedys bedzie INSTAGRAM</Text>
+      <Text>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
