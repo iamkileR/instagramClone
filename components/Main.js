8a642239d03d32029ac7eaa5aac72e2d17d90a4f
iamkileR <jakubsadza@gmail.com>
2022-01-08 20:08:11 +0100

    Follow System
    Update database with Follow / Unfollow
    Fetching Users Being Followed

--- components/Main.js
+++ components/Main.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import {View, Text } from 'react-native'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { fetchUser, fetchUserPosts } from '../redux/actions/index'
+import {fetchUser, fetchUserPosts, fetchUserFollowing} from '../redux/actions/index'
 import { createMaterialBottomTabNavigator  } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import firebase from 'firebase'
@@ -22,6 +22,7 @@ export class MainScreen extends Component {
     componentDidMount(){
         this.props.fetchUser();
         this.props.fetchUserPosts();
+        this.props.fetchUserFollowing();
     }
 
     render() {
@@ -73,6 +74,6 @@ const mapStateToProps = (store) => ({
     currentUser: store.userState.currentUser
 })
 
-const mapDispatchProps = (dispatch) => bindActionCreators({fetchUser, fetchUserPosts}, dispatch);
+const mapDispatchProps = (dispatch) => bindActionCreators({fetchUser, fetchUserPosts, fetchUserFollowing}, dispatch);
 
 export default connect(mapStateToProps, mapDispatchProps)(MainScreen);
